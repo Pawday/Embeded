cf38f10953cdd714a57b6f1b7fa400d37d42f76b
Pawday <pawday@mail.ru>
2023-02-21 04:25:31 +0300

    Rename cmake-toolchains to llvm-toolchains

--- cmake-toolchains/llvm-arm-cortex-m3.cmake
+++
@@ -1,9 +0,0 @@
-set(CMAKE_IS_INIT_CACHE_INVOCATION TRUE)
-
-#TODO[cmake]: pass -triple arm-none-eabi -tune-cpu cortex-m3 options to llvm toolchain loader
-
-# On first run it disables double inclusion for this toolchain file
-set(CMAKE_TOOLCHAIN_FILE FALSE)
-
-set(CMAKE_SYSTEM_NAME Generic)
-set(CMAKE_SYSTEM_CUSTOM_CODE "include(${CMAKE_CURRENT_LIST_DIR}/LoadLLVMToolchain.cmake)")
